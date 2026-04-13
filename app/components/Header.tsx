1734ba37646c5260bb1ddbff85c80e8ce7c9f12e
Sumit Giri <sumit.g@yoekisoft.in>
2026-04-13 14:27:10 +0530

    ui

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -9,6 +9,7 @@ import { useState } from 'react';
 export function Header() {
   const [isOpen, setIsOpen] = useState(false);
   return (
+    
     <header className="bg-[#10162F] text-white sticky top-0 z-50 relative">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
