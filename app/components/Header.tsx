984cf4f7ab6425ccf069e48ac791e3027e2e5a9a
Sumit Giri <sumit.g@yoekisoft.in>
2026-04-13 12:39:16 +0530

    bug fixed

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -25,7 +25,6 @@ export function Header() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           {/* Logo */}
-          
           <div className="flex items-center gap-8">
             <Link href={ROUTES.HOME} className="flex items-center gap-2 hover:opacity-90 transition-opacity" >
               <Image src={logo.src} alt="CodeLearn Logo" width={44} height={32} className="rounded"/>
