d6d0812705815a8a9089f61e963a88a9f50a024e
Sumitgiriyoeki <sumit.g@yoekisoft.in>
2026-05-01 15:05:28 +0530

    Updates

---
+++ _next/static/kR2tNGdWYV8ZU-Ir1Ii4_/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fcategory\u002F[slug]","\u002Fcourse\u002F[courseId]","\u002Fcourse\u002F[courseId]\u002Fdocument\u002F[documentId]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
